d4cc37cd76c79069b48e24be6b3ae16339d0303c
Alexey Shmalko <rasen.dubi@gmail.com>
2015-12-27 09:08:25 +0200

    Add check for OOM case in __rust_reallocate

--- linkmem/src/lib.rs
+++ linkmem/src/lib.rs
@@ -12,7 +12,7 @@ extern crate smalloc;
 use smalloc::Smalloc;
 use core::intrinsics::copy_nonoverlapping;
 
-
+// TODO(rasen): allow importing this from different module (use weak linkage or extern)
 static mut allocator: Smalloc = Smalloc {
     start: 0 as *mut u8,
     size: 0,
@@ -26,25 +26,27 @@ pub fn init(alloc: Smalloc) {
 }
 
 #[no_mangle]
-pub extern fn __rust_allocate(size: usize, _align: usize) -> *mut u8 {
-    unsafe { allocator.alloc(size) }
+pub unsafe extern fn __rust_allocate(size: usize, _align: usize) -> *mut u8 {
+    allocator.alloc(size)
 }
 
 #[no_mangle]
-pub extern fn __rust_deallocate(ptr: *mut u8, _old_size: usize, _align: usize) {
-    unsafe { allocator.free(ptr) }
+pub unsafe extern fn __rust_deallocate(ptr: *mut u8, _old_size: usize, _align: usize) {
+    allocator.free(ptr)
 }
 
+// TODO(tailhook): optimize me
 #[no_mangle]
-pub extern fn __rust_reallocate(ptr: *mut u8, old_size: usize, size: usize,
-                                _align: usize) -> *mut u8 {
-    // TODO(tailhook): optimize me
-    unsafe {
-        let nval = allocator.alloc(size);
-        copy_nonoverlapping(ptr, nval, old_size);
-        allocator.free(ptr);
-        return nval;
+pub unsafe extern fn __rust_reallocate(ptr: *mut u8, old_size: usize, size: usize,
+                                       _align: usize) -> *mut u8 {
+    let new = allocator.alloc(size);
+    if new.is_null() {
+        return ::core::ptr::null_mut();
     }
+
+    copy_nonoverlapping(ptr, new, old_size);
+    allocator.free(ptr);
+    new
 }
 
 #[no_mangle]
@@ -55,5 +57,5 @@ pub extern fn __rust_reallocate_inplace(_ptr: *mut u8, old_size: usize,
 
 #[no_mangle]
 pub extern fn __rust_usable_size(size: usize, _align: usize) -> usize {
-    return size;
+    size
 }
